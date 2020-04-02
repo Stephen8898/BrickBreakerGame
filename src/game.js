76ae7ba6a28f3b23ef84e5018be5595bfad5280b
Stephen <stephen_s@Stephens-MacBook-Pro.local>
2020-04-02 10:52:44 -0400

    added levels and level hanlder to update geme levels

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 import Paddel from './paddel.js';
 import InputHanlder from './input.js';
 import Ball from './ball.js';
-import {buildLevel, level1} from './levels.js';
+import {buildLevel, level1, level2} from './levels.js';
 // import Brick from './brick.js';
 
 
@@ -10,7 +10,8 @@ const GAMESTATE = {
     PAUSED: 0,
     RUNNING: 1,
     MENU: 2,
-    GAMEOVER: 3 
+    GAMEOVER: 3,
+    NEWLEVEL: 4
 
 }
 
@@ -22,26 +23,39 @@ export default class Game {
         //Game menu state
         this.gamestate = GAMESTATE.MENU;
         this.gameObjects = [];
+        this.bricks = [];
          // instance of the paddel object
-         this.paddel = new Paddel(this); 
-         this.ball = new Ball(this);
-    
+        this.paddel = new Paddel(this); 
+        this.ball = new Ball(this);
+        
         this.lives = 4;
+
+        this.levels = [level1,level2];
+
+        this.currentLevel = 0;
 
         new InputHanlder(this.paddel, this);
     }
     
     start(){ 
-        if(this.gamestate !== GAMESTATE.MENU) return;
-
-        let bricks = buildLevel(this, level1);
-        this.gameObjects = [this.ball, this.paddel, ...bricks];
+        if(this.gamestate !== GAMESTATE.MENU 
+            && this.gamestate !== GAMESTATE.NEWLEVEL
+            ) return;
+        this.bricks = buildLevel(this, this.levels[this.currentLevel]);
+        this.ball.reset();
+        this.gameObjects = [this.ball, this.paddel];
 
         this.gamestate = GAMESTATE.RUNNING;
     }
 
 
     update(deltaTime){
+        // Handels game level by cheking brick array length
+        if(this.bricks.length === 0){
+            this.currentLevel++;
+            this.gamestate = GAMESTATE.NEWLEVEL;
+            this.start();
+        }
 
         if(this.lives === 0) this.gamestate = GAMESTATE.GAMEOVER;
 
@@ -51,18 +65,24 @@ export default class Game {
             ) return;
 
         this.gameObjects.forEach(object => object.update(deltaTime));
+        this.bricks.forEach(brick => brick.update(deltaTime));
         document.getElementById('lives').innerHTML = this.lives;
 
-       this.gameObjects = this.gameObjects.filter(object => !object.destoryBrick);
+       this.bricks = this.bricks.filter(brick => !brick.destoryBrick);
+
     }
 
     draw(context){
         //draws game objects
         this.gameObjects.forEach(object => object.draw(context));
+        this.bricks.forEach(brick => brick.draw(context));
 
         if(this.gamestate === GAMESTATE.PAUSED){
            document.getElementById("Game-state").innerHTML = "Game Paused";
         }else if(this.gamestate == GAMESTATE.MENU){
+            context.rect(0,0, this.gameWidth, this.gameHeight);
+            context.fillStyle = "rgba(0,0,0,1)"
+             context.fill();
             document.getElementById("Game-state").innerHTML = "Press Shift to Start";
             document.getElementById('lives').innerHTML = this.lives;
             // context.fillStyle = '#1a0000';
